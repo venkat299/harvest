f9849bdb54e3e74d090e3fc687d0ff66a2083adc
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2018-01-22 18:40:05 +0530

    added test case for urndl

---
+++ harvest/tests/urndl/test_urndl.py
@@ -0,0 +1,154 @@
+import datetime
+from unittest import skip
+from django.urls import reverse
+from django.test import TestCase
+from import_export import resources
+from tablib import Dataset
+import datetime
+from django.utils import timezone
+import logging
+log = logging.getLogger(__name__)
+
+from django.conf import settings
+from harvest.models.strategy import Strategy, Stock, Watchlist, Ledger, Order, Signal
+#import harvest.tests.LogThisTestCase as LogThisTestCase
+
+class StockResource(resources.ModelResource):
+    class Meta:
+        model = Stock
+        exclude = ('id',)
+        import_id_fields = ('isin',)
+
+def create_strategy(strategy_name):
+    return Strategy.objects.create(name=strategy_name, status="ACTIVE")
+
+def add_stocks_to_watchlist(csv_path):
+        #refer: https://simpleisbetterthancomplex.com/packages/2016/08/11/django-import-export.html
+        stock_resource = StockResource()
+        dataset = Dataset()
+        with open(csv_path) as new_stocks:
+        # new_stocks = settings.BASE_DIR+'/harvest/test_data/eod_sample.csv'
+            imported_data = dataset.load(new_stocks.read())
+            stock_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+
+class URNDL(TestCase):
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):    #refer: https://stackoverflow.com/a/14306659
+        # populating tables
+        print("populating tables")
+        strategy = create_strategy(strategy_name="URNDL")
+        add_stocks_to_watchlist(settings.BASE_DIR+ settings.DATA_DIRECTORY+'/eod_sample_1.csv')
+        Ledger.objects.create(credit=20000, closing=20000,desc="URNDL/CASHIN",strategy=strategy, timestamp=timezone.now())
+        super(URNDL, cls).setUpClass()
+
+        log.info("\n ======== Strategy ========")
+        for item in Strategy.objects.all():
+            log.info(item)
+
+    # def test_strategy_view_with_an_entry(self):
+    #     """
+    #     test the web page for strategy
+    #     """
+    #     response = self.client.get(reverse('harvest:strategy'))
+    #     self.assertQuerysetEqual(
+    #         response.context['user_strategy_list'],
+    #         ['<Strategy: URNDL__ACTIVE>']
+    #     )
+    #     self.assertContains(response, '''>URNDL</a>
+    #         </td>
+    #         <td>ACTIVE</td>''')
+
+
+    @skip('time consuming')
+    def test_train_strategy(self):
+
+        # test train()
+        response = self.client.get(reverse('harvest:strategy_train', kwargs={'strategy_id': 1}))
+        self.assertContains(response, '''"success": true''')
+        log.info("\n ======== Watchlist ========")
+        for item in Watchlist.objects.filter(strategy__name='URNDL'):
+            log.info(item)
+
+
+    # @skip('time consuming')
+    def test_strategy_api(self):
+
+        # test train()
+        response = self.client.get(reverse('harvest:strategy_train', kwargs={'strategy_id': 1}))
+        self.assertContains(response, '''"success": true''')
+
+        # test execute()
+        response = self.client.get(reverse('harvest:strategy_predict', kwargs={'strategy_id': 1}))
+        self.assertContains(response, '''"success": true''')
+
+        log.info("\n ======== Watchlist ========")
+        for item in Watchlist.objects.filter(strategy__name='URNDL'):
+            log.info(item)
+
+        log.info("\n ======== Signals ========")
+        for item in Signal.objects.filter(watchlist__strategy__name='URNDL'):
+            log.info(item)
+
+        order =Order.objects.get(signal__watchlist__strategy__name='URNDL', signal__watchlist__stock__stock='3IINFOTECH', status='PENDING')
+        uuid = order.uuid
+        avg_price = order.price
+        response = self.client.get(reverse('harvest:strategy_confirm_order', kwargs={'uuid': uuid, 'avg_price':avg_price}))
+        self.assertContains(response, '''"success": true''')
+
+        order =Order.objects.get(signal__watchlist__strategy__name='URNDL', signal__watchlist__stock__stock='ASHOKLEY', status='PENDING')
+        uuid = order.uuid
+        avg_price = order.price
+        response = self.client.get(reverse('harvest:strategy_confirm_order', kwargs={'uuid': uuid, 'avg_price':avg_price}))
+        self.assertContains(response, '''"success": true''')
+
+        uuid = Order.objects.get(signal__watchlist__strategy__name='URNDL', signal__watchlist__stock__stock='DABUR', status='PENDING').uuid
+        response = self.client.get(reverse('harvest:strategy_cancel_order', kwargs={'uuid': uuid}))
+        self.assertContains(response, '''"success": true''')
+
+        stock_1 = Stock.objects.get(stock='3IINFOTECH', series='EQ')
+        stock_1.close = 2500
+        stock_1.save()
+        stock_2 = Stock.objects.get(stock='ASHOKLEY', series='EQ')
+        stock_2.close = 150
+        stock_2.save()
+
+        response = self.client.get(reverse('harvest:strategy_predict', kwargs={'strategy_id': 1}))
+        self.assertContains(response, '''"success": true''')
+
+        order =Order.objects.get(signal__watchlist__strategy__name='URNDL', signal__watchlist__stock__stock='ASHOKLEY', status='PENDING')
+        uuid = order.uuid
+        avg_price = order.price
+        response = self.client.get(reverse('harvest:strategy_confirm_order', kwargs={'uuid': uuid, 'avg_price':avg_price}))
+        self.assertContains(response, '''"success": true''')
+
+        uuid = Order.objects.get(signal__watchlist__strategy__name='URNDL', signal__watchlist__stock__stock='3IINFOTECH', status='PENDING').uuid
+        response = self.client.get(reverse('harvest:strategy_cancel_order', kwargs={'uuid': uuid}))
+        self.assertContains(response, '''"success": true''')
+
+        #prematurly closing a signal 
+        watchlist_id1= Watchlist.objects.get(stock=stock_1, strategy__name='URNDL').id
+        response = self.client.get(reverse('harvest:close_signal', kwargs={'watchlist_id': watchlist_id1}))
+        self.assertContains(response, '''"success": true''')
+
+        log.info("\n ======== Watchlist ========")
+        for item in Watchlist.objects.filter(strategy__name='URNDL'):
+            log.info(item)
+
+        log.info("\n ======== Signals ========")
+        for item in Signal.objects.filter(watchlist__strategy__name='URNDL'):
+            log.info(item)
+
+        log.info("\n ======== Order ========")
+        for item in Order.objects.filter(signal__watchlist__strategy__name='URNDL'):
+            log.info(item)
+
+        log.info("\n ======== Ledger ========")
+        for item in Ledger.objects.filter(strategy__name='URNDL'):
+            log.info(item)
+
+    
--- harvest/views/strategy.py
+++ harvest/views/strategy.py
@@ -3,36 +3,45 @@ from django.http import JsonResponse
 from harvest.services.risky52d.main import  Risky52D
 from harvest.services.urndl.main import  URNDL
 from harvest.services import order
+from harvest.models.strategy import Strategy
 
 def train(request, strategy_id):
+    strategy_id = int(strategy_id)
     response = {"success":False}
+    strategy_name = Strategy.objects.get(id=strategy_id).name
+    print(strategy_name)
 
-    if strategy_id is "1":
+    if strategy_name == "RISKY52D":
         response = Risky52D.train()
-    if strategy_id is "2":
+    if strategy_name == "URNDL":
         response = URNDL.train()
 
+    # print(response)
     final = JsonResponse(response, safe=False)
-    print(final)
     return final
 
 def predict(request, strategy_id):
     response = {"success":False}
+    strategy_id = int(strategy_id)
+    strategy_name = Strategy.objects.get(id=strategy_id).name
 
-    if strategy_id is "1":
+    if strategy_name == "RISKY52D":
         response = Risky52D.predict()
-    if strategy_id is "2":
+    if strategy_name == "URNDL":
         response = URNDL.predict()
 
+    # print(response)   
     final = JsonResponse(response, safe=False)
     return final
 
 def reset(request, strategy_id):
     response = {"success":False}
+    strategy_id = int(strategy_id)
+    strategy_name = Strategy.objects.get(id=strategy_id).name
 
-    if strategy_id is "1":
+    if strategy_name == "RISKY52D":
         response = Risky52D.reset()
-    if strategy_id is "2":
+    if strategy_name == "URNDL":
         response = URNDL.reset()
 
     final = JsonResponse(response, safe=False)
